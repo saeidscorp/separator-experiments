dfea1d5cb4466bc3c45bdae6b6371d67bb3d25c3
Saeid Akbari <saeidscorp@yahoo.com>
2020-12-27 11:41:08 +0330

    tree separator now queries the oracle. its own query mechanisms still not implemented.

--- alg/TreeSeparator.cpp
+++ alg/TreeSeparator.cpp
@@ -25,9 +25,10 @@ TreeSeparator<bidirectional_graph>::TreeSeparator(const model::Graph<bidirection
 template<bool bidirectional_graph>
 void TreeSeparator<bidirectional_graph>::preprocess(Oracle<bidirectional_graph> *oracle) {
     auto starting_count = oracle->queries();
-    auto all_nodes = oracle->getGraph()->getNodes();
+    auto graph = oracle->getGraph();
+    auto all_nodes = graph->getNodes();
 
-    CentroidDecomposition cd{oracle->getGraph()};
+    CentroidDecomposition cd{graph};
 
     auto bfs = [&](model::Node *root, int max_size) {
 
@@ -47,7 +48,7 @@ void TreeSeparator<bidirectional_graph>::preprocess(Oracle<bidirectional_graph> 
 
             auto node = queue.front();
             queue.pop();
-            this->selected_nodes.push_back(node);  // FIXME: make sure what needs to be put in selected_nodes
+            this->selected_nodes.push_back(*graph->getNode(node->getId()));
             this->marked[node->getId()] = true;
             current_size++;
 
@@ -67,22 +68,23 @@ void TreeSeparator<bidirectional_graph>::preprocess(Oracle<bidirectional_graph> 
                        return std::make_pair(p->getId(), false);
                    });
 
-    auto dfs = util::recursive_lambda([&](auto &&dfs, model::Node *node, model::Node *parent) -> bool {
+    auto fill_nodes = util::recursive_lambda([&](auto &&fill_nodes, model::Node *node, model::Node *parent) -> bool {
         auto marked_subtrees_count = 0;
         for (auto neighbor : node->getNeighs()) {
             if (neighbor == parent) continue;
-            if (dfs(neighbor, node))
+            if (fill_nodes(neighbor, node))
                 marked_subtrees_count++;
         }
-        if (marked_subtrees_count > 1) {
+        auto marked = this->marked[node->getId()];
+        if (marked_subtrees_count > 1 && !marked) {
             this->selected_nodes.push_back(node);
             this->marked[node->getId()] = true;
         }
-        return marked_subtrees_count || this->marked[node->getId()];
+        return marked_subtrees_count || marked;
     });
 
-    auto original_root = oracle->getGraph()->getNodes()[0];
-    dfs(original_root, original_root);
+    auto original_root = all_nodes[0];
+    fill_nodes(original_root, original_root);
 
     auto new_size = this->selected_nodes.size();
     std::cout << "selected nodes went from " << selected_count << " to " << new_size
@@ -90,38 +92,49 @@ void TreeSeparator<bidirectional_graph>::preprocess(Oracle<bidirectional_graph> 
 
     auto sum_of_path_lengths = 0;
 
-//    Node *prev = nullptr;
-//    for (auto i = 0ul; i < this->_seps_count; ++i) {
-//        auto current = this->selected_nodes[i];
-//        if (prev) {
-//
-//            auto place_in_table = [this, &sum_of_path_lengths](const auto &node_pair, const auto &result) {
-//                if (!result) return;
-//                auto result_path_len = result.value().first.size();
-//                auto result_eta = result.value().second;
-//
-//                sum_of_path_lengths += result_path_len;
-//                this->table.emplace(node_pair, result_eta);
-//            };
-//
-//            endpoints node_pair{prev, current};
-//            auto result = oracle->query(node_pair);
-//            place_in_table(node_pair, result);
-//
-//            decltype(result) result_rev;
-//
-//            if constexpr (!bidirectional_graph) {
-//                endpoints node_pair_rev{current, prev};
-//                result_rev = oracle->query(node_pair_rev);
-//                place_in_table(node_pair_rev, result);
-//            }
-//
-//            if (!(result || result_rev))
-//                std::cerr << "there is a disconnection between the linear graph's nodes." << std::endl;
-//
-//        }
-//        prev = current;
-//    }
+    auto place_in_table = [this, &sum_of_path_lengths](const auto &node_pair, const auto &result) {
+        if (!result) return;
+        auto result_path_len = result.value().first.size();
+        auto result_eta = result.value().second;
+
+        sum_of_path_lengths += result_path_len;
+        this->table.emplace(node_pair, result_eta);
+    };
+
+    auto dfs = util::recursive_lambda([&](auto &&dfs, model::Node *node,
+                                          model::Node *parent,
+                                          model::Node *prev) -> void {
+
+        auto marked = false;
+        if (this->marked[node->getId()])
+            marked = true;
+
+        if (marked && prev != nullptr) {
+            endpoints node_pair{prev, node};
+            auto result = oracle->query(node_pair);
+            place_in_table(node_pair, result);
+
+            decltype(result) result_rev;
+
+            if constexpr (!bidirectional_graph) {
+                endpoints node_pair_rev{node, prev};
+                result_rev = oracle->query(node_pair_rev);
+                place_in_table(node_pair_rev, result);
+            }
+
+            if (!(result || result_rev))
+                std::cerr << "there is a disconnection between the tree's nodes." << std::endl;
+        }
+
+        if (marked)
+            prev = node;
+
+        for (auto neighbor : node->getNeighs())
+            if (neighbor != parent)
+                dfs(neighbor, node, prev);
+    });
+
+    dfs(original_root, original_root, nullptr);
 
     this->_avg_path_length = (ETA) sum_of_path_lengths / (this->_seps_count - 1);
     this->preprocessing_num_queries = oracle->queries() - starting_count;
